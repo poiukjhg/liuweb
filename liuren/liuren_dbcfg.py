ccedfd4f17b3be062446c0b6361298adba292883
zz <zuoyipeng@foxmail.com>
2017-05-16 10:13:20 +0800

    blank

---
+++ liuren/liuren_dbcfg.py
@@ -0,0 +1,9 @@
+from peewee import *
+database =  MySQLDatabase(
+        database = 'liuq',
+        host = 'localhost',
+        port = 3306, 
+        user = 'zz',
+        passwd = '1234',
+        charset = 'utf8'
+        ) 
--- liuren/run.py
+++ liuren/run.py
@@ -22,7 +22,7 @@ def run(fortell_time_str, fortell_zone_str, fortell_index_str, gender, birth):
 	tmptodayeb = tmptoday[3:]
 	tmphour = gt.get_hour()
 	tmphoureb = tmphour[3:]
-	tmpgeneral = gt.get_month_general()
+	tmpgeneral = gt.get_month_general()	
 	if gender == '1':
 		tmpgender = '男'
 	else:
@@ -37,9 +37,10 @@ def run(fortell_time_str, fortell_zone_str, fortell_index_str, gender, birth):
 	'''
 	#fortell_time_str = fortell_time.strftime('%Y-%m-%d %H:%M:%S')
 	gyl = get_yearloop(tmpgender, tmpbirth, fortell_time_str[0:4])
+	tmpblank = gt.get_blank()
 	tmpline = "<p>"+tmpgender+'命  '+gyl.return_res()+"</p>"
 	tmpline = tmpline+"<p>起课时间："+	fortell_time_str+"</p>"
-	tmpline = tmpline+"<p>占时: "+gt.get_year()+"年 "+gt.get_month()+"月 "+tmptoday+"日 "+tmphour+ "时 " + " 月将"+ tmpgeneral+gt.get_blank()+"</p>"
+	tmpline = tmpline+"<p>占时: "+gt.get_year()+"年 "+gt.get_month()+"月 "+tmptoday+"日 "+tmphour+ "时 " + " 月将"+ tmpgeneral+tmpblank+"</p>"
 	tmpline = tmpline+"<p>"+gt.get_front_jieqi()+" "+gt.get_after_jieqi()+"</p>"
 	gs = generate_result(tmptodayst,tmptodayeb,tmpgeneral,tmphoureb)
 	gs.generate_skyplate()
@@ -56,5 +57,7 @@ def run(fortell_time_str, fortell_zone_str, fortell_index_str, gender, birth):
 	global_record_list.set_global_record(3, tmp_month[3:])
 	global_record_list.set_global_record(6, tmphour[0:3])
 	global_record_list.set_global_record(7, tmphour[3:])
+	global_record_list.set_global_record(58, tmpblank[17:20])
+	global_record_list.set_global_record(59, tmpblank[20:])
 	print global_record_list.get_global_record()
 	return tmpline+output11.output()
